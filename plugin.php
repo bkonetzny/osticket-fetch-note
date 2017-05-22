d6cd39feca61072101e82a7d7f303125b59e33d9
Bastian Konetzny <bkonetzny@users.noreply.github.com>
2017-05-22 21:26:38 +0200

    Create plugin.php

---
+++ plugin.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+    'id' =>             'osticket:fetch-note', 
+    'version' =>        '1.0.0',
+    'name' =>           'Fetch Note',
+    'author' =>         'Bastian Konetzny',
+    'description' =>    'Fetch content for an issue note on ticket creation.',
+    'url' =>            'https://github.com/bkonetzny/osticket-fetch-note',
+    'plugin' =>         'fetch-note.php:FetchNotePlugin',        
+);
